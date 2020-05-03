11080bc5aa0930ddff465a6b5e247ed44a8ce32b
Alex Ross <arossg17@gmail.com>
2020-05-03 14:33:59 -0400

    Add profile image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,19 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+    <div class="profile-background col-lg-4">
+        <img src="~assets/me.jpg" alt="Alex" class="profile-image">
+    </div>
   </q-page>
 </template>
+
+<style scoped>
+.profile-image {
+  width: 10rem;
+  border-radius:50%;
+  margin:2.5rem auto;
+  box-shadow:1px 1px 0.7rem rgba(0,0,0,0.2)
+}
+</style>
 
 <script>
 export default {
